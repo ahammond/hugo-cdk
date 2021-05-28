c168a4d535a86d18748a2e1ffc0dcc0d03fe8505
Andrew Hammond <andrew.hammond@helix.com>
2021-05-28 12:41:21 -0700

    remove eslint:fix target - redundant

--- .projenrc.js
+++ .projenrc.js
@@ -4,14 +4,13 @@ const project = new AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'hugo-cdk',
 
-  cdkDependencies: ['aws-cdk-lib'], /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  cdkDependencies: ['aws-cdk-lib'] /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */,
   cdkVersionPinning: true,
-  deps: ['aws-lambda', 'source-map-support'], /* Runtime dependencies of this module. */
+  deps: ['aws-lambda', 'source-map-support'] /* Runtime dependencies of this module. */,
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: ['@types/aws-lambda'], /* Build dependencies for this module. */
+  devDeps: ['@types/aws-lambda'] /* Build dependencies for this module. */,
   // packageName: undefined,            /* The "name" in package.json. */
-  projectType: AwsCdkTypeScriptApp.projectType, /* Which type of project this is (library/app). */
+  projectType: AwsCdkTypeScriptApp.projectType /* Which type of project this is (library/app). */,
   // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
 });
-project.addTask('eslint:fix', { exec: 'eslint --fix' });
 project.synth();
