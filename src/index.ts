df68b6f4564068f8dd6b89d4af9c64d46e6337e5
Andrew Hammond <andrew.george.hammond@gmail.com>
2021-10-16 16:20:16 -0700

    feat: codecov should alarm

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './cloudfront-redirect';
 export * from './hugo-site';
 // export * from './standard-redirects-for-cloudfront';
 export * from './static-site';
+export * from './breaking';
