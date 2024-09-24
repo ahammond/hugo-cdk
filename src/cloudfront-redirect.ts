3844d553dbe27ba87d8543c556410581789dacc5
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2024-09-24 10:29:53 -0700

    lambda updated

--- src/cloudfront-redirect.ts
+++ src/cloudfront-redirect.ts
@@ -1,9 +1,30 @@
+import {
+  CompositePrincipal,
+  ManagedPolicy,
+  Role,
+  ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
+import { Runtime, Version } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+// import { PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class CloudfrontRedirect extends NodejsFunction {
+  readonly functionVersion: Version;
+
   constructor(scope: Construct) {
+    const role = new Role(scope, 'CloudfrontRedirectRole', {
+      assumedBy: new CompositePrincipal(
+        new ServicePrincipal('lambda.amazonaws.com'),
+        new ServicePrincipal('edgelambda.amazonaws.com')
+      ),
+    });
+    role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName(
+        'service-role/AWSLambdaBasicExecutionRole'
+      )
+    );
+
     super(scope, 'cloudfront-redirect', {
       bundling: {
         externalModules: [
@@ -11,25 +32,33 @@ export class CloudfrontRedirect extends NodejsFunction {
           '@types/aws-lambda',
         ],
       },
+      role,
+      runtime: Runtime.NODEJS_20_X,
     });
+
+    this.functionVersion = new Version(scope, 'RedirectLambdaVersion', {
+      lambda: this,
+      description: 'Version for CloudFront redirect function',
+    });
+
     // Grant the Lambda@Edge service principal permission to assume this role
-    this.grantPrincipal.addToPrincipalPolicy(
-      new PolicyStatement({
-        actions: ['sts:AssumeRole'],
-        principals: [new ServicePrincipal('edgelambda.amazonaws.com')],
-      })
-    );
+    // this.grantPrincipal.addToPrincipalPolicy(
+    //   new PolicyStatement({
+    //     actions: ['sts:AssumeRole'],
+    //     principals: [new ServicePrincipal('edgelambda.amazonaws.com')],
+    //   })
+    // );
 
     // Add the required Lambda@Edge permissions
-    this.addToRolePolicy(
-      new PolicyStatement({
-        actions: [
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:PutLogEvents',
-        ],
-        resources: ['arn:aws:logs:*:*:*'],
-      })
-    );
+    // this.addToRolePolicy(
+    //   new PolicyStatement({
+    //     actions: [
+    //       'logs:CreateLogGroup',
+    //       'logs:CreateLogStream',
+    //       'logs:PutLogEvents',
+    //     ],
+    //     resources: ['arn:aws:logs:*:*:*'],
+    //   })
+    // );
   }
 }
--- test/cloudfront-redirect.test.ts
+++ test/cloudfront-redirect.test.ts
@@ -1,14 +1,63 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as CuT from '../src';
 
 test('cloudfront-redirect basic functionality', () => {
   const app = new App();
   const stack = new Stack(app, 'TestStack');
   // WHEN
-  new CuT.CloudfrontRedirect(stack, 'Test');
+  new CuT.CloudfrontRedirect(stack);
   // THEN
-  expectCDK(stack).to(haveResource('AWS::IAM::Role'));
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function'));
-  expectCDK(stack).to(haveResource('AWS::Lambda::Version'));
+  const template = Template.fromStack(stack);
+
+  const roles = template.findResources('AWS::IAM::Role', {
+    Properties: {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: 'sts:AssumeRole',
+            Effect: 'Allow',
+            Principal: { Service: 'lambda.amazonaws.com' },
+          },
+          {
+            Action: 'sts:AssumeRole',
+            Effect: 'Allow',
+            Principal: { Service: 'edgelambda.amazonaws.com' },
+          },
+        ],
+        Version: '2012-10-17',
+      },
+      ManagedPolicyArns: [
+        {
+          'Fn::Join': [
+            '',
+            [
+              'arn:',
+              { Ref: 'AWS::Partition' },
+              ':iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+            ],
+          ],
+        },
+      ],
+    },
+  });
+  expect(roles).toBeDefined();
+  const roleKeys = Object.keys(roles);
+  expect(roleKeys.length).toEqual(1);
+  const roleRef = roleKeys[0];
+
+  const functions = template.findResources('AWS::Lambda::Function', {
+    Properties: {
+      Role: { 'Fn::GetAtt': [roleRef, 'Arn'] },
+      Runtime: 'nodejs20.x',
+    },
+  });
+  expect(functions).toBeDefined();
+  const functionKeys = Object.keys(functions);
+  expect(functionKeys.length).toEqual(1);
+  const functionRef = functionKeys[0];
+
+  template.hasResourceProperties('AWS::Lambda::Version', {
+    FunctionName: { Ref: functionRef },
+  });
 });
